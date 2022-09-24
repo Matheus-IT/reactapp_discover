baeb57b08bb8b562fca89ed6c4405cad016d07b7
matheus costa <matheuscosta.tech@gmail.com>
2022-09-23 23:14:19 -0300

    Organizing folder structure and styles

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,11 @@
+import './styles.css';
+
+export default function App() {
+  return (
+    <div className="container">
+      <h1>Attendance list</h1>
+      <input type="text" placeholder="Enter your name..."/>
+      <button type="button">Add</button>
+    </div>
+  );
+}
